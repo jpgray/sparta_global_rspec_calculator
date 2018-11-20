81ac7f767fe59424e191f6ad7bc3d5105bd08c31
Jon Gray <jgray@spartaglobal.com>
2018-11-20 15:19:16 +0000

    v1.0 refactored for efficiency

--- spec/rspec_calculator_spec.rb
+++ spec/rspec_calculator_spec.rb
@@ -1,20 +1,29 @@
 require 'spec_helper'
 
 describe 'Calcengine' do
-  calc = Calcengine.new
+  # can use before(:each) instead to reinstantiate before every test. (:suite) will run before all tests in your test suite. after() will also work as expected.
+  before(:all) do
+    @calc = Calcengine.new
+  end
+
+
   it "should correctly add two numbers" do
-    expect(calc.add(1,2)).to eq 3
+    expect(@calc.add(1,2)).to eq 3
   end
 
   it "should correctly subtract one number from another" do
-    expect(calc.subtract(1,2)).to eq -1
+    expect(@calc.subtract(1,2)).to eq -1
   end
 
   it "should correctly multiply two numbers" do
-    expect(calc.multiply(1,2)).to eq 2
+    expect(@calc.multiply(1,2)).to eq 2
   end
 
   it "should correctly divide one number by another" do
-    expect(calc.divide(1,2)).to eq 0.5
+    expect(@calc.divide(1,2)).to eq 0.5
+  end
+  
+  after(:each) do
+    puts "what a great test!"
   end
 end
